0ec06df68f8d599af6be06d7dc35952f6e0f4e46
willyc <willcowendrums@gmail.com>
2022-02-07 16:56:22 +0000

    all cards added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,108 @@
+const ul = document.querySelector('.cards')
+function createCard(pokemonName) {
 
-console.log(data);
+    //card as a list item
+    const li = document.createElement('li')
+    li.setAttribute('class', 'card')
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    //pokemon name
+    const h2 = document.createElement('h2')
+    h2.setAttribute('class', 'card--title')
+    h2.innerText = pokemonName
+
+
+    //pokemon image
+    const img = document.createElement('img');
+    img.setAttribute('class', 'card--image')
+
+
+    //card text
+    const cardText = document.createElement('ul')
+    cardText.setAttribute('class', 'card--text')
+
+    //li elements of card text
+    const cardTextHP = document.createElement('li')
+    cardTextHP.innerText = 'HP: '
+
+    const cardTextAttack = document.createElement('li')
+    cardTextAttack.innerText = 'ATTACK: '
+
+    const cardTextDefense = document.createElement('li')
+    cardTextDefense.innerText = 'DEFENSE: '
+
+    const cardTextSpecialAttack = document.createElement('li')
+    cardTextSpecialAttack.innerText = 'SPECIAL-ATTACK: '
+
+    const cardTextSpecialDefense = document.createElement('li')
+    cardTextSpecialDefense.innerText = 'SPECIAL-DEFENSE: '
+
+    const cardTextSpeed = document.createElement('li')
+    cardTextSpeed.innerText = 'SPEED: '
+
+    for (let i = 0; i < data.length; i++) {
+        const pokemon = data[i];
+        if (pokemon.name === pokemonName) {
+            img.setAttribute('src', pokemon.sprites.other['official-artwork'].front_default) 
+        }
+        const stats = pokemon.stats
+        for (let j = 0; j < stats.length; j++) {
+            if (pokemon.name === pokemonName) {
+                if (stats[j].stat.name === "hp") {
+            cardTextHP.innerText += stats[j]["base_stat"]
+            }
+            if (stats[j].stat.name === "attack") {
+                cardTextAttack.innerText += stats[j]["base_stat"]
+            }
+            if (stats[j].stat.name === "defense") {
+                cardTextDefense.innerText += stats[j]["base_stat"]
+            }
+            if (stats[j].stat.name === "special-attack") {
+                cardTextSpecialAttack.innerText += stats[j]["base_stat"]
+            }
+            if (stats[j].stat.name === "special-defense") {
+                cardTextSpecialDefense.innerText += stats[j]["base_stat"]
+            }
+            if (stats[j].stat.name === "speed") {
+                cardTextSpeed.innerText += stats[j]["base_stat"]
+            }
+        }
+    }
+    }
+
+    //appending
+    ul.append(li)
+    li.append(h2)
+    li.append(img)
+    li.append(cardText)
+    cardText.append(cardTextHP)
+    cardText.append(cardTextAttack)
+    cardText.append(cardTextDefense)
+    cardText.append(cardTextSpecialAttack)
+    cardText.append(cardTextSpecialDefense)
+    cardText.append(cardTextSpeed)
+}
+
+createCard('bulbasaur');
+createCard('ivysaur');
+createCard('venusaur');
+createCard('charmander');
+createCard('charmeleon');
+createCard('charizard');
+createCard('squirtle');
+createCard('wartortle');
+createCard('blastoise');
+createCard('caterpie');
+createCard('metapod');
+createCard('butterfree');
+createCard('weedle');
+createCard('kakuna');
+createCard('beedrill');
+createCard('pidgey');
+createCard('pidgeotto');
+createCard('pidgeot');
+createCard('rattata');
+createCard('raticate');
+
+
+
+
